6c2f1b20a4334fdcbb6fe1991ab74292a4ad69fb
mich8638 <freddie.sackur@rackspace.co.uk>
2017-11-02 11:59:06 +0000

    Improved output of proxy commands without function body

--- ProxyGenerator.ps1
+++ ProxyGenerator.ps1
@@ -6,11 +6,22 @@ $EmptyReplacer = {
 }
 
 $BodyRegex = [regex]::new('begin.*(?=<#)', [System.Text.RegularExpressions.RegexOptions]::Singleline)
-$CommentRegex = [regex]::new('<#.*#>', [System.Text.RegularExpressions.RegexOptions]::Singleline)
+$CommentRegex = [regex]::new('[^\n]*<#.*#>', [System.Text.RegularExpressions.RegexOptions]::Singleline)
 
-Get-Command -Module NetTCPIP | select -First 1 | foreach {
+Get-Command -Module NetTCPIP | foreach {
     $Text = New-ProxyCommand -Name $_.Name
-    $Text = $Regex.Replace($P, $EmptyReplacer)   #remove function body
+    $Text = $Regex.Replace($Text, $EmptyReplacer)   #remove function body
+
+    #Move comment block to the top
+    $CommentBlock = $CommentRegex.Match($Text).Value
+    $Text = $CommentBlock, $CommentRegex.Replace($Text, $EmptyReplacer) -join "`r`n"
+
+    #move param block closing paren to new line
+    $Text = $Text -replace '\)\s*$', "`r`n)"
+
+    #Indent by 4-space. Also replace CR, as piping to clip turns CRLF into double-spaced lines
     $Text = $Text -split '\r\n' | foreach {"`n    $_"}
-    "function " + $_.Name + "`n{`n" + $Text + "`n}`n`n" | Out-String
+
+    #Out
+    "function " + $_.Name + "`n{" + $Text + "`n}`n`n" | Out-String
 }
